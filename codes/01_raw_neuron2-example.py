e73c7ea376b3c1df5f5b9ee0011a6a36c0e0b736
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2026-05-31 18:26:47 +0300

    Add files via upload

---
+++ codes/01_raw_neuron2-example.py
@@ -0,0 +1,31 @@
+import math
+import random
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+class Neuron:
+    def __init__(self):
+        # soruda verilen hatalı değerler
+        # self.w1 = 0.2
+        # self.bias = -3
+
+        # bizim bulduğumuz olması gereken kaysayılar.
+        self.w1 = 1.74
+        self.bias = -16.48
+
+    def forward(self, x1):
+        z = x1 * self.w1 + self.bias
+        return sigmoid(z)
+
+
+if __name__ == "__main__":
+    neuron = Neuron()
+    soru = 8
+    predict = neuron.forward(soru)
+    if predict < 0.5:
+        print(f"{soru} soru ile %{predict * 100:.2f} olasılıkla KALDI.")
+    else:
+        print(f"{soru} soru ile %{predict * 100:.2f} olasılıkla GEÇTİ.")
