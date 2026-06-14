6745115b49fc9c65d07e2a541cf6a713d57711d1
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2026-06-14 17:37:06 +0300

    Add files via upload

---
+++ codes/03_loss_function.py
@@ -0,0 +1,40 @@
+import math
+import random
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+def square_error(predict, target):
+    return (predict - target) ** 2
+
+
+class Neuron:
+    def __init__(self):
+        self.w1 = random.uniform(-1, 1)
+        self.w2 = random.uniform(-1, 1)
+        self.w3 = random.uniform(-1, 1)
+        self.bias = random.uniform(-1, 1)
+
+    def forward(self, x1, x2, x3):
+        z = x1 * self.w1 + x2 * self.w2 + x3 * self.w3 + self.bias
+        return sigmoid(z)
+
+
+if __name__ == "__main__":
+    neuron = Neuron()
+    rooms = 3
+    area = 150
+    age = 2
+    target = 0.58
+    predict = neuron.forward(rooms, area, age)
+    error = square_error(predict, target)
+    print(f"rooms w1 = {neuron.w1:.4f}")
+    print(f"area  w2 = {neuron.w2:.4f}")
+    print(f"age   w3 = {neuron.w3:.4f}")
+    print(f"bias     = {neuron.bias:.4f}")
+    print(f"--------------------------\n")
+    print(f"Gerçek Değer : {target}\n")
+    print(f"Tahmin : {predict}\n")
+    print(f"Karesel Hata : {error}\n")
